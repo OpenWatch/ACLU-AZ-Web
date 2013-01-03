c8f4cc29ef85beaab8b6e5b223ef7e1dbf339925
Rich Jones <rich@anomos.info>
2013-01-03 14:40:13 -0800

    filtering, flagging

--- acluazweb/acluazweb/urls.py
+++ acluazweb/acluazweb/urls.py
@@ -8,7 +8,14 @@ urlpatterns = patterns('',
     url(r'^$', 'report.views.root', name='root'),
     url(r'^map/', 'report.views.map', name='map'),
     url(r'^new/', 'report.views.new', name='new'),
+    url(r'^all/', 'report.views.all', name='all'),
+    url(r'^approved/', 'report.views.approved', name='approved'),
     url(r'^contact/', 'report.views.contact', name='contact'),
     url(r'^r/(?P<rid>[a-zA-Z0-9_. -]+)', 'report.views.report', name='report'),
+    url(r'^approve/(?P<rid>[a-zA-Z0-9_. -]+)', 'report.views.approve', name='approve'),
+    url(r'^flag/(?P<rid>[a-zA-Z0-9_. -]+)', 'report.views.flag', name='flag'),
+
     url(r'^admin/', include(admin.site.urls)),
+
+
 )
--- acluazweb/report/views.py
+++ acluazweb/report/views.py
@@ -10,7 +10,21 @@ from report.models import Report, ReportForm
 
 def root(request):
 
-    reports = Report.objects.all()
+    reports = Report.objects.filter(red_flagged=False)
+
+    return render_to_response('home.html', { 'reports': reports, 'reportactive': True, 'numtotal': len(reports), 'numweek': 0} , 
+        context_instance=RequestContext(request))
+
+def all(request):
+
+    reports = Report.objects.filter()
+
+    return render_to_response('home.html', { 'reports': reports, 'reportactive': True, 'numtotal': len(reports), 'numweek': 0} , 
+        context_instance=RequestContext(request))
+
+def approved(request):
+
+    reports = Report.objects.filter(approved=True)
 
     return render_to_response('home.html', { 'reports': reports, 'reportactive': True, 'numtotal': len(reports), 'numweek': 0} , 
         context_instance=RequestContext(request))
@@ -21,6 +35,30 @@ def report(request, rid):
 
     return render_to_response('report.html', { 'report': report, 'reportactive': True, } , 
         context_instance=RequestContext(request))
+
+def approve(request, rid):
+
+    report = get_object_or_404(Report, pk=rid)
+
+    if report.approved:
+        report.approved = False
+    else:
+        report.approved = True
+    report.save()
+
+    return HttpResponseRedirect('/r/' + str(report.pk))
+
+def flag(request, rid):
+
+    report = get_object_or_404(Report, pk=rid)
+
+    if report.red_flagged:
+        report.red_flagged = False
+    else:
+        report.red_flagged = True
+    report.save()
+
+    return HttpResponseRedirect('/r/' + str(report.pk))
 
 def map(request):
 
